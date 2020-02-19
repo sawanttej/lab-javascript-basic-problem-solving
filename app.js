d2c2632dae2656a0f050259e5a51f622746ca2ea
sawanttej <61007735+sawanttej@users.noreply.github.com>
2020-02-19 13:29:18 +0530

    prograd Id

--- app.js
+++ app.js
@@ -6,11 +6,32 @@
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
 
+var string1 = "lalit"
+console.log(sring1);
+
+var string = "sarang"
+console.log(string2);
+
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
+
+let driver_name = string1,
+    lenght;
+Navigator_name = string2, lenght;
+
+
+if (driver_name => Navigator_name) {
+    console.log(driver_name)
+} else {
+    console.log(Navigator_name)
+}s
+
+
+
+
 
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the number of name, and also print the vowel letters along with the vowel characters. or
@@ -18,38 +39,113 @@
 
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
-// - Print the number of lower case characters
+// - Print the number of lower case character
 
 
-// Progression 3: Control Statements - 2
-// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
 
-// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+val name[15] = {
+    's',
+    'a',
+    'r',
+    'a',
+    'n',
+    'g'
+};
 
-// 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
-// - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
 
-// 3.3 Depending on the lexicographic order of the strings, print:
-// - The driver's name goes first.
-// - Yo, the navigator goes first definitely.
-// - What?! You both have the same name?
 
-// Bonus Time!
-// Bonus 1:
-// Go to lorem ipsum generator and:
-// Generate 3 paragraphs. Store the text in a variable type of string.
-// Make your program count the number of words in the string.
-// Make your program count the number of times the Latin word et appears.
-// Bonus 2:
-// Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
-// "A man, a plan, a canal, Panama!"
-// "Amor, Roma"
-// "race car"
-// "stack cats"
-// "step on no pets"
-// "taco cat"
-// "put it up"
-// "Was it a car or a cat I saw?" and "No 'x' in Nixon".
 
-// Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' 
+let vowels;
+vowels = consonant = digits = space = 0;
+
+for (int i = 0; name[15]; i++) {
+
+    if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i == 'u') {
+            console.log(vowels);
+        } else {
+            console.log(consonant);
+            S
+        }
+        elseif((name[i] >= 'a' && name[i] <= 'z') || (name[i] = 'A' && name[i] <= 'Z')) {
+            console.log(consonant)
+        }
+
+        elseif((name[i] >= '0' && name[i] <= '14')) {
+            console.log(digit)
+        }
+
+        elseif(name[i] == ' ')
+
+
+
+    }
+
+    // Progression 3: Control Statements - 2
+    // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
+    var string = "sarang"
+    let first_number, second_number;
+    let first_number = second_number = counter = 0;
+    for (counter = 0; string[counter] = '\0'; counter++) {
+        if (str[counter] >= 'A' && str[counter] <= 'Z') {
+            console.log(first_number);
+        } else {
+            console.log(second_number)
+        }
+
+
+    }
+
+
+    // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+    var string1 = "sarang"
+    console.log(reverse.str)
+
+    // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
+
+    var string1 = "sarang"
+    var string2 = "lalit"
+    merge = string1 + string2;
+    console.log(merge)
+
+
+    // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
+
+
+
+    // 3.3 Depending on the lexicographic order of the strings, print:
+    // - The driver's name goes first.
+    // - Yo, the navigator goes first definitely.
+    // - What?! You both have the same name?
+    let number1 = string1.length;
+    number2 = string2.length;
+
+    if (number1 > number2) {
+        console.log(string1);
+    } else {
+        console.log(string2);
+    }
+    elseif {
+        console.log(same_name)
+    }
+
+
+    // Bonus Time!
+    // Bonus 1:
+    // Go to lorem ipsum generator and:
+    // Generate 3 paragraphs. Store the text in a variable type of string.
+    // Make your program count the number of words in the string.
+    // Make your program count the number of times the Latin word et appears.
+    // Bonus 2:
+    // Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
+
+    // "A man, a plan, a canal, Panama!"
+    // "Amor, Roma"
+    // "race car"
+    // "stack cats"
+    // "step on no pets"
+    // "taco cat"
+    // "put it up"
+    // "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+
+    // Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' 
